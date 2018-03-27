001df26daaad6d7ccf9de32c1a464202d8152970
Will Ockmore <will.ockmore@gmail.com>
2018-03-27 21:20:39 +0100

    add check for correct url in build script

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-set -e
+set -euo pipefail
+
+# check for the url being set correctly in config
+if [[ ! -z $(ag localhost _config.yml) ]]; then
+  echo 'The url param is set to localhost in _config.yml - comment out the correct line!!'
+  exit 1
+fi
 
 jekyll build
 cp -r _site/** build/
